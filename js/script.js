a2b9912a036607e53aa44ba82ad30af76623dd55
Thilde <thilde.persson@gmail.com>
2025-03-03 15:32:54 +0100

    Knapp för upg1 - labb3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
     alert("jQuery fungerar!");
 });
+
+document.getElementById('showMessageButton').addEventListener('click', function() {
+    alert('Hello, JavaScript!');
+});
