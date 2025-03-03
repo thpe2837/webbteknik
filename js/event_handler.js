d32c5bba2f98fcbde46e06ba275ef4bcf009dabb
Thilde <thilde.persson@gmail.com>
2025-03-03 20:12:12 +0100

    css för knapp och array med event för upg 2

---
+++ js/event_handler.js
@@ -0,0 +1,35 @@
+/** Hanterar uppdatering av event på första-sidan */
+
+// Array med events
+const events = [
+	{
+		title: "Knytkalas på Bäcknäset",
+		date: "2025-08-24",
+		image: "../img/knytkalas.png",
+		description: "Välkommen på knytkalas den 24 augusti nere vid hamnen i Bäcknäset! Ta med något gott att äta och dela med andra. Vi njuter av sensommarkvällen tillsammans med god mat, trevligt sällskap och fin utsikt över vattnet."
+	},
+	{
+		title: ,
+		date: ,
+		image: ,
+		description:
+	},
+	{
+		title: ,
+		date: ,
+		image: ,
+		description:
+	},
+	{
+		title: ,
+		date: ,
+		image: ,
+		description:
+	},
+	{
+		title: ,
+		date: ,
+		image: ,
+		description:
+	}
+]
