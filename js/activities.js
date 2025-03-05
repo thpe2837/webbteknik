213f3f564cbd1db8cf1244595976796cfb3de957
Thilde <thilde.persson@gmail.com>
2025-03-05 11:04:17 +0100

    Korrigerat så att eventen hamnar på rätt plats i kalendern

--- js/activities.js
+++ js/activities.js
@@ -1,5 +1,34 @@
+/** Animation för beskrivning av event */
 $(document).ready(function () {
     $(".label-event").click(function () {
-        $(this).next(".event-description").slideToggle();
+        let desc = $(this).next(".event-description");
+        $(".event-description").not(desc).fadeOut(200); // Dölj andra beskrivningar
+        desc.fadeToggle(200);
+    });
+});
+
+/** Automatisk uträkning av posiion av event */
+$(document).ready(function () {
+    $(".event").each(function () {
+        let dateText = $(this).find(".label-event").text(); 
+        let match = dateText.match(/(\d+) (\w+)/); // Extrahera datumet
+
+        if (match) {
+            let day = parseInt(match[1]);
+            let month = match[2].toLowerCase();
+
+            let monthDays = { 
+                "januari": 0, "februari": 31, "mars": 59, "april": 90, "maj": 120,
+                "juni": 151, "juli": 181, "augusti": 212, "september": 243,
+                "oktober": 273, "november": 304, "december": 334
+            };
+
+            if (month in monthDays) {
+                let daysIntoYear = monthDays[month] + day; 
+                let angle = (daysIntoYear / 365) * 360 - 90; // -90 så att 1 jan startar högst upp
+
+                $(this).css("--angle", angle + "deg");
+            }
+        }
     });
 });
